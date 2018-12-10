1178eb534ac5f90494cd723da0b3a4d87cf9ccc3
hareeshops <42886985+hareeshops@users.noreply.github.com>
2018-12-10 12:55:23 +0530

    addition method added

---
+++ shell_faq/operators.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Author : Hareesh Arava
+
+
+echo "******Addition******"
+echo -n "Enter first number: "
+read num1 
+echo -n "Enter second number: "
+read num2
+sum=$((num1+num2))
+echo $sum
