12c46b09deea17f608e9e524188551881a57956f
hareeshops <42886985+hareeshops@users.noreply.github.com>
2018-12-10 12:56:58 +0530

    substraction method added

--- shell_faq/operators.sh
+++ shell_faq/operators.sh
@@ -10,3 +10,12 @@ echo -n "Enter second number: "
 read num2
 sum=$((num1+num2))
 echo $sum
+
+
+echo "******Subtraction******"
+echo -n "Enter first number: "
+read num1 
+echo -n "Enter second number: "
+read num2
+sub=$((num1-num2))
+echo $sub
